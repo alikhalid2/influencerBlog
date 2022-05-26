68f701340ae57402ca60ee6cd0f729b082aa29a6
EngYoussef1 <101152968+EngYoussef1@users.noreply.github.com>
2022-05-26 21:05:13 +0200

    new

--- Project/view/src/Components/User/User.jsx
+++ Project/view/src/Components/User/User.jsx
@@ -9,14 +9,14 @@ import "./user.css";
 export default function User() {
   return (
     <div className="user">
-      <div className="userimg">
-      <img
+      
+      <img className="userimg"
         src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?cs=srgb&dl=pexels-pixabay-220453.jpg&fm=jpg"
         alt="user"
-        width="40px"
-        height="40px"
+        width="100px"
+        height="100px"
       />
-      </div>
+     
       <p className="username">username</p>
 
       <i className="singlePostIcon fa-regular fa-trash-can deliticon"></i>
