513d4a3553d24c16e6da11cc19135d5925785dcf
alikhalid2 <83635110+alikhalid2@users.noreply.github.com>
2022-05-27 14:48:21 +0200

    names

--- Project/view/src/Pages/About/About.jsx
+++ Project/view/src/Pages/About/About.jsx
@@ -15,10 +15,10 @@ export default function About() {
           <ul  className="names">
           <li className="namslist">youssef ashrf abd algfar salm</li>
           <li className="namslist">ahmed adel ahmed hassan alkfafy</li>
-          <li className="namslist">ali khaled ali swelam</li>
+          <li className="namslist">ali khaled ali swilam</li>
           <li className="namslist"> ahmed mahamed ahmed hamed</li>
-          <li className="namslist">slah hassen sliman</li>
-          <li className="namslist">ghader ahmed</li>
+          <li className="namslist">salah soliman hussein soliman</li>
+          <li className="namslist">ghadeer ahmed elsalhawy</li>
          
           </ul>
       </div>
