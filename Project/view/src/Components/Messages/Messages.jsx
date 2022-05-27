aa4baf179170881ae6b4a8b08b31e1c767e50aed
EngYoussef1 <101152968+EngYoussef1@users.noreply.github.com>
2022-05-27 17:57:35 +0200

    massage

--- Project/view/src/Components/Messages/Messages.jsx
+++ Project/view/src/Components/Messages/Messages.jsx
@@ -2,10 +2,19 @@
 import React from "react";
 
 // importing other components
-
+import Massage from"../Massage/Massage.jsx";
 // importing style sheet
 import "./messages.css";
 
 export default function Messages() {
-  return <div className="messages">messages</div>;
+  return( 
+  <div className="messages">
+    <Massage/>
+    <Massage/>
+    <Massage/>
+    <Massage/>
+    <Massage/>
+    <Massage/>
+  </div>
+  );
 }
